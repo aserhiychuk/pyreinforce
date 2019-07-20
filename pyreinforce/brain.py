20a068b5b17e1de9c2810fd5844fa2d9f5025d24
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-07-20 16:54:33 -0400

    Make brain more generic

--- pyreinforce/brain.py
+++ pyreinforce/brain.py
@@ -1,28 +1,14 @@
-import tensorflow as tf
 
 
 class Brain(object):
     '''
     TODO Brain class
     '''
-    def __init__(self, sess=None):
-        super().__init__()
-
-        self._sess = sess
-
-    def train(self, batch):
-        raise NotImplementedError('Subclasses must implement {}#train() method'.format(type(self).__name__))
+    def train(self, *args, **kwargs):
+        raise NotImplementedError
 
     def __enter__(self):
-        if not self._sess:
-            self._sess = tf.Session()
-
-        self._sess.__enter__()
-
-        init = tf.global_variables_initializer()
-        self._sess.run(init)
-
         return self
 
     def __exit__(self, *args):
-        self._sess.__exit__(*args)
+        pass
