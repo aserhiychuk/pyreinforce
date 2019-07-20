6b208f98f64a9e58e367ca783c24e01ede5a14b7
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-07-20 10:54:59 -0400

    Build python package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyReinforce',
+    version='0.1',
+    description='Deep Reinforcement Learning library for Python',
+    author='Anton Serhiychuk',
+    url='https://github.com/aserhiychuk/pyreinforce',
+    license='MIT',
+    install_requires=[
+        'numpy>=1.0'
+    ],
+    extras_require={
+        'gym': ['gym'],
+        'tensorflow': ['tensorflow>=1.0']
+    },
+    packages=find_packages()
+)
