c9df1ba28c9c063f01857cc576b878ed99eda65f
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-22 17:19:40 -0500

    Document core module

--- pyreinforce/core.py
+++ pyreinforce/core.py
@@ -4,25 +4,59 @@ import numpy as np
 
 
 class Agent(object):
-    '''
-    TODO Agent class
-    '''
+    """Base class for all Reinforcement Learning agents."""
+
     def __init__(self):
         super().__init__()
 #         self._logger = logging.getLogger('{}.{}'.format(__name__, type(self).__name__))
 
     def seed(self, seed=None):
+        """Seed the random number generator.
+
+        Parameters
+        ----------
+        seed : int, optional
+            Seed for random number generator.
+
+        Note
+        ----
+        This implementation does nothing.
+        """
         pass
 
     def run(self):
+        """Train the agent.
+
+        Note
+        ----
+        This implementation does nothing.
+        """
         pass
 
 
 class SimpleAgent(Agent):
-    '''
-    TODO Simple Agent class
-    '''
+    """Abstract agent that implements a typical RL flow."""
+
     def __init__(self, n_episodes, env, converter=None, callback=None):
+        """
+        Parameters
+        ----------
+        n_episodes : int
+            Number of episodes to train the agent for.
+        env : obj
+            Environment
+        converter : Converter, optional
+            If specified, allows to pre/post process state, action, or experience.
+        callback : callable, optional
+            If specified, is called after each episode
+            with the following parameters:
+                cur_episode : int
+                    Current episode number.
+                n_episodes : int
+                    Total number of episodes.
+                rewards : list
+                    List of cumulative rewards obtained during prior episodes.
+        """
         super().__init__()
         self._n_episodes = n_episodes
         self._env = env
@@ -32,6 +66,15 @@ class SimpleAgent(Agent):
         self._global_step = 0
 
     def run(self):
+        """Train the agent by running `self._n_episodes` episodes.
+
+        Returns
+        -------
+        list of floats
+            List of cumulative rewards per episode
+        obj
+            Some useful training statistics
+        """
         rewards = []
         stats = []
 
@@ -57,6 +100,13 @@ class SimpleAgent(Agent):
         return rewards, stats
 
     def _run_episode(self, i):
+        """Run a single episode.
+
+        Parameters
+        ----------
+        i : int
+            Current episode number.
+        """
         cur_step = 0
         reward = 0
         done = False
@@ -102,20 +152,73 @@ class SimpleAgent(Agent):
         return reward
 
     def _reset(self):
+        """Bring the environment to its initial state.
+
+        Returns
+        -------
+        obj
+            Initial state.
+        """
         s = self._env.reset()
 
         return s
 
     def _step(self, a):
+        """Take step in the environment by performing action `a` on it.
+
+        Parameters
+        ----------
+        a int or obj
+            Action to be performed.
+
+        Returns
+        -------
+        obj
+            Next state after performing action `a`.
+        float
+            Reward achieved by action `a`.
+        bool
+            Flag that indicates if the episode has terminated.
+        dict or obj
+            Diagnostic information useful for debugging.
+        """
         s1, r, done, info = self._env.step(a)
 
         return s1, r, done, info
 
     def _before_episode(self, i=0):
+        """Called before an episode starts.
+
+        Parameters
+        ----------
+        i : int, optional
+            Current episode number.
+        """
         pass
 
     def _act(self, s, cur_step=0, cur_episode=0):
-        pass
+        """Return action given state `s`.
+
+        Parameters
+        ----------
+        s
+            Current state
+        cur_step : int, optional
+            Current step of the episode
+        cur_episode : int, optional
+            Current episode
+
+        Returns
+        -------
+        int or obj
+            Action to be performed.
+
+        Note
+        ----
+        Must be implemented by subclasses. Default implementation
+        raises `NotImplementedError` exception
+        """
+        raise NotImplementedError
 
     def _create_experience(self, **kwargs):
         s = kwargs['s']
@@ -127,7 +230,28 @@ class SimpleAgent(Agent):
         return (s, a, r, s1, s1_mask)
 
     def _observe(self, experience):
+        """Called after taking a step in the environment.
+
+        Subclasses can use this method to save current `experience`
+        in replay memory and/or perform a training step.
+
+        Parameters
+        ----------
+        experience : tuple
+            Tuple that contains information about the current step:
+            s
+                State before performing action
+            a : int or obj
+                Action that was performed
+            r : float
+                Reward achieve by the action
+            s1
+                State after performing action
+            s1_mask : int
+                0 if `s1` is a terminal state, 1 otherwise.
+        """
         pass
 
     def _after_episode(self):
+        """Called after an episode ends."""
         pass
