e4090cb86a6b067dd066c0f3f880262ba4153ba9
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-24 09:42:04 -0500

    Document brain module

--- pyreinforce/brain.py
+++ pyreinforce/brain.py
@@ -1,10 +1,23 @@
 
 
 class Brain(object):
-    '''
-    TODO Brain class
-    '''
+    """Hold neural network(s) implementation.
+
+    The purpose of this class is to encapsulate neural network(s)
+    implementation from the agent. Agents typically use it to
+    obtain/predict possible actions, and then train the network.
+
+    Please refer to the `/examples` folder for some possible
+    usages of the `Brain` class.
+    """
+
     def train(self, *args, **kwargs):
+        """Perform a training step.
+
+        Note
+        ----
+        Subclasses must implement this method.
+        """
         raise NotImplementedError
 
     def __enter__(self):
