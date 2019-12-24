cf7f8b0f541e1b6052e9f4537adf908ffe52b3e2
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-24 09:17:27 -0500

    Document utils module

--- pyreinforce/utils.py
+++ pyreinforce/utils.py
@@ -2,6 +2,20 @@ import numpy as np
 
 
 def discount_rewards(rewards, gamma):
+    """Compute discounted rewards.
+
+    Parameters
+    ----------
+    rewards : list
+        List of rewards obtained from the environment.
+    gamma : float
+        Discount factor, must be between 0 and 1.
+
+    Returns
+    -------
+    list
+        List of discounted rewards.
+    """
     result = np.empty_like(rewards, dtype=np.float32)
     g = 0
 
