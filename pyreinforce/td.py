ecf09588487766213b9986e19b435d7c73c9491d
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-25 15:45:30 -0500

    Document td module

--- pyreinforce/td.py
+++ pyreinforce/td.py
@@ -4,11 +4,39 @@ from pyreinforce.core import SimpleAgent
 
 
 class TdAgent(SimpleAgent):
-    '''
-    TODO Temporal Difference Agent class
-    '''
+    """Agent that implements Temporal Difference algorithm."""
+
     def __init__(self, n_episodes, env, brain, acting, replay_memory, gamma,
                  converter=None, train_freq=1, callback=None):
+        """
+        Parameters
+        ----------
+        n_episodes : int
+            Number of episodes to train the agent for.
+        env : obj
+            Environment.
+        brain : Brain
+            DQN.
+        acting : ActingPolicy
+            Action selection policy.
+        replay_memory : Memory
+            Experience replay memory.
+        gamma : float
+            Discount factor, must be between 0 and 1.
+        converter : Converter, optional
+            If specified, allows to pre/post process state, action, or experience.
+        train_freq : int, optional
+            Training frequency.
+        callback : callable, optional
+            If specified, is called after each episode
+            with the following parameters:
+                cur_episode : int
+                    Current episode number.
+                n_episodes : int
+                    Total number of episodes.
+                rewards : list
+                    List of cumulative rewards obtained during prior episodes.
+        """
         super().__init__(n_episodes, env, converter, callback)
         self._brain = brain
         self._acting = acting
@@ -17,18 +45,60 @@ class TdAgent(SimpleAgent):
         self._train_freq = train_freq
 
     def seed(self, seed=None):
+        """Seed action selection policy and experience replay memory.
+
+        Parameters
+        ----------
+        seed : int, optional
+            Seed for random number generator.
+        """
         super().seed(seed)
 
         self._acting.seed(seed)
         self._replay_memory.seed(seed)
 
     def _act(self, s, cur_step=0, cur_episode=0):
+        """Choose action given current state of the environment.
+
+        Parameters
+        ----------
+        s : obj
+            Current state of the environment.
+        cur_step : int, optional
+            Current step within episode.
+        cur_episode : int, optional
+            Current episode.
+
+        Returns
+        -------
+        int
+            Action according to action selection policy.
+        """
         q = self._predict_q(s, cur_step=cur_step)
         a = self._acting.act(q, cur_step=cur_step, cur_episode=cur_episode, n_episodes=self._n_episodes)
 
         return a
 
     def _predict_q(self, states, **kwargs):
+        """Predict `Q`-values given states.
+
+        Parameters
+        ----------
+        states : array
+            Array of states.
+        **kwargs
+            Additional keyword arguments.
+
+        Returns
+        -------
+        array
+            Array of `Q`-values for each state.
+
+        Raises
+        ------
+        AssertionError
+            If `Q`-values contain nan or inf.
+        """
         q = self._brain.predict_q(states, **kwargs)
 
         assert not np.isnan(q).any(), 'Q contains nan: {}'.format(q)
@@ -37,6 +107,13 @@ class TdAgent(SimpleAgent):
         return q
 
     def _observe(self, experience):
+        """Store experience in the replay memory and perform a training step.
+
+        Parameters
+        ----------
+        experience : tuple
+            Tuple of (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         self._replay_memory.add(experience)
 
         if self._global_step % self._train_freq == 0:
@@ -46,6 +123,13 @@ class TdAgent(SimpleAgent):
                 self._train(batch)
 
     def _train(self, batch):
+        """Perform a training step.
+
+        Parameters
+        ----------
+        batch : list
+            List of tuples (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         batch = np.array(batch)
         batch = np.reshape(batch, (-1, batch.shape[-1]))
 
@@ -59,6 +143,17 @@ class TdAgent(SimpleAgent):
                           train_freq=self._train_freq)
 
     def _get_targets(self, batch, q, q1):
+        """Compute TD targets for current states `s`.
+
+        Parameters
+        ----------
+        batch : list
+            List of tuples (`s`, `a`, `r`, `s1`, `s1_mask`).
+        q : array
+            `Q`-values for current states `s`.
+        q1 : array
+            `Q`-values for next states `s1`.
+        """
         target = np.empty_like(q)
         target[:] = q
         a = np.int32(batch[:, 1])
@@ -70,4 +165,5 @@ class TdAgent(SimpleAgent):
         return target
 
     def _after_episode(self):
+        """Make sure all experiences are stored in the replay memory."""
         self._replay_memory.flush()
