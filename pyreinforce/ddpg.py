9ef8c006aa2b943de516448aaf6c7031aee38af4
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-25 16:30:40 -0500

    Document ddpg module

--- pyreinforce/ddpg.py
+++ pyreinforce/ddpg.py
@@ -4,11 +4,39 @@ from pyreinforce.core import SimpleAgent
 
 
 class DdpgAgent(SimpleAgent):
-    '''
-    TODO DDPG Agent class
-    '''
+    """Agent that implements Deep Deterministic Policy Gradient algorithm."""
+
     def __init__(self, n_episodes, env, brain, acting, replay_memory, gamma,
                  converter=None, train_freq=1, callback=None):
+        """
+        Parameters
+        ----------
+        n_episodes : int
+            Number of episodes to train the agent for.
+        env : obj
+            Environment.
+        brain : Brain
+            Holder for both actor and critic.
+        acting : ActingPolicy
+            Action selection policy.
+        replay_memory : Memory
+            Experience replay memory.
+        gamma : float
+            Discount factor, must be between 0 and 1.
+        converter : Converter, optional
+            If specified, allows to pre/post process state, action, or experience.
+        train_freq : int, optional
+            Training frequency.
+        callback : callable, optional
+            If specified, is called after each episode
+            with the following parameters:
+                cur_episode : int
+                    Current episode number.
+                n_episodes : int
+                    Total number of episodes.
+                rewards : list
+                    List of cumulative rewards obtained during prior episodes.
+        """
         super().__init__(n_episodes, env, converter, callback)
         self._brain = brain
         self._acting = acting
@@ -17,18 +45,62 @@ class DdpgAgent(SimpleAgent):
         self._train_freq = train_freq
 
     def seed(self, seed=None):
+        """Seed action selection policy and experience replay memory.
+
+        Parameters
+        ----------
+        seed : int, optional
+            Seed for random number generator.
+        """
         super().seed(seed)
 
         self._acting.seed(seed)
         self._replay_memory.seed(seed)
 
     def _act(self, s, cur_step=0, cur_episode=0):
+        """Choose action given current state of the environment.
+
+        Parameters
+        ----------
+        s : obj
+            Current state of the environment.
+        cur_step : int, optional
+            Current step within episode.
+        cur_episode : int, optional
+            Current episode.
+
+        Returns
+        -------
+        int
+            Action according to action selection policy.
+        """
         a = self._predict_a(s, False, cur_step=cur_step)
         a = self._acting.act(a, cur_step=cur_step)
 
         return a
 
     def _predict_a(self, states, is_target=False, **kwargs):
+        """Predict actions given states.
+
+        Parameters
+        ----------
+        states : array
+            Array of states.
+        is_target : bool, optional
+            Use primary network if False, and target one otherwise.
+        **kwargs
+            Additional keyword arguments.
+
+        Returns
+        -------
+        array
+            Array of actions.
+
+        Raises
+        ------
+        AssertionError
+            If actions contain nan or inf.
+        """
         a = self._brain.predict_a(states, is_target, **kwargs)
 
         assert not np.isnan(a).any(), 'A contains nan: {}'.format(a)
@@ -37,6 +109,29 @@ class DdpgAgent(SimpleAgent):
         return a
 
     def _predict_q(self, states, actions, is_target=False, **kwargs):
+        """Predict `Q`-values given states and actions.
+
+        Parameters
+        ----------
+        states : array
+            Array of states.
+        actions : array
+            Array of actions.
+        is_target : bool, optional
+            Use primary network if False, and target one otherwise.
+        **kwargs
+            Additional keyword arguments.
+
+        Returns
+        -------
+        array
+            Array of `Q`-values.
+
+        Raises
+        ------
+        AssertionError
+            If `Q`-values contain nan or inf.
+        """
         q = self._brain.predict_q(states, actions, is_target, **kwargs)
 
         assert not np.isnan(q).any(), 'Q contains nan: {}'.format(q)
@@ -45,6 +140,13 @@ class DdpgAgent(SimpleAgent):
         return q
 
     def _observe(self, experience):
+        """Store experience in the replay memory and perform a training step.
+
+        Parameters
+        ----------
+        experience : tuple
+            Tuple of (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         self._replay_memory.add(experience)
 
         if self._global_step % self._train_freq == 0:
@@ -54,6 +156,13 @@ class DdpgAgent(SimpleAgent):
                 self._train(batch)
 
     def _train(self, batch):
+        """Perform a training step.
+
+        Parameters
+        ----------
+        batch : list
+            List of tuples (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         batch = np.array(batch)
         batch = np.reshape(batch, (-1, batch.shape[-1]))
 
@@ -73,4 +182,5 @@ class DdpgAgent(SimpleAgent):
                           train_freq=self._train_freq)
 
     def _after_episode(self):
+        """Make sure all experiences are stored in the replay memory."""
         self._replay_memory.flush()
