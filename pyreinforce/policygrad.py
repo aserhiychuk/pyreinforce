399beaf50894579880b651c91c2bb8e1bc32da20
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2019-12-25 16:10:15 -0500

    Document policygrad module

--- pyreinforce/policygrad.py
+++ pyreinforce/policygrad.py
@@ -5,10 +5,35 @@ from pyreinforce.utils import discount_rewards
 
 
 class PolicyGradientAgent(SimpleAgent):
-    '''
-    TODO Policy Gradient Agent class
-    '''
-    def __init__(self, n_episodes, env, brain, acting, gamma, converter=None, callback=None):
+    """Agent that implements Policy Gradient algorithm."""
+
+    def __init__(self, n_episodes, env, brain, acting, gamma,
+                 converter=None, callback=None):
+        """
+        Parameters
+        ----------
+        n_episodes : int
+            Number of episodes to train the agent for.
+        env : obj
+            Environment.
+        brain : Brain
+            Neural network.
+        acting : ActingPolicy
+            Action selection policy.
+        gamma : float
+            Discount factor, must be between 0 and 1.
+        converter : Converter, optional
+            If specified, allows to pre/post process state, action, or experience.
+        callback : callable, optional
+            If specified, is called after each episode
+            with the following parameters:
+                cur_episode : int
+                    Current episode number.
+                n_episodes : int
+                    Total number of episodes.
+                rewards : list
+                    List of cumulative rewards obtained during prior episodes.
+        """
         super().__init__(n_episodes, env, converter, callback)
         self._brain = brain
         self._acting = acting
@@ -16,17 +41,59 @@ class PolicyGradientAgent(SimpleAgent):
         self._episode_memory = []
 
     def seed(self, seed=None):
+        """Seed action selection policy.
+
+        Parameters
+        ----------
+        seed : int, optional
+            Seed for random number generator.
+        """
         super().seed(seed)
 
         self._acting.seed(seed)
 
     def _act(self, s, cur_step=0, cur_episode=0):
+        """Choose action given current state of the environment.
+
+        Parameters
+        ----------
+        s : obj
+            Current state of the environment.
+        cur_step : int, optional
+            Current step within episode.
+        cur_episode : int, optional
+            Current episode.
+
+        Returns
+        -------
+        int
+            Action according to action selection policy.
+        """
         probs = self._predict_policy(s, cur_step=cur_step)
         a = self._acting.act(probs)
 
         return a
 
     def _predict_policy(self, states, **kwargs):
+        """Predict actions probabilities given states.
+
+        Parameters
+        ----------
+        states : array
+            Array of states.
+        **kwargs
+            Additional keyword arguments.
+
+        Returns
+        -------
+        array
+            Array of actions probabilities for each state.
+
+        Raises
+        ------
+        AssertionError
+            If actions probabilities contain nan or inf.
+        """
         probs = self._brain.predict_policy(states, **kwargs)
 
         assert not np.isnan(probs).any(), 'policy contains nan: {}'.format(probs)
@@ -35,9 +102,23 @@ class PolicyGradientAgent(SimpleAgent):
         return probs
 
     def _observe(self, experience):
+        """Store experience in the episode memory.
+
+        Parameters
+        ----------
+        experience : tuple
+            Tuple of (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         self._episode_memory.append(experience)
 
     def _train(self, batch):
+        """Perform a training step.
+
+        Parameters
+        ----------
+        batch : array
+            Array of experiences (`s`, `a`, `r`, `s1`, `s1_mask`).
+        """
         states = np.stack(batch[:, 0])
         actions = batch[:, 1].reshape(-1, 1)
         returns = batch[:, 2].reshape(-1, 1)
@@ -45,6 +126,7 @@ class PolicyGradientAgent(SimpleAgent):
         self._brain.train(states, actions, returns)
 
     def _after_episode(self):
+        """Compute discounted rewards and perform a training step."""
         episode = np.array(self._episode_memory)
         episode[:, 2] = discount_rewards(episode[:, 2], self._gamma)
 
