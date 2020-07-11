385faca9a45ca2c4fc2602503607035f2ef91906
Anton Serhiychuk <30370802+aserhiychuk@users.noreply.github.com>
2020-07-11 08:33:24 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyReinforce',
-    version='0.1',
+    version='0.2',
     description='Deep Reinforcement Learning library for Python',
     author='Anton Serhiychuk',
     url='https://github.com/aserhiychuk/pyreinforce',
